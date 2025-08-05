31887dbb5abbe06e3223983a8509bf875907932c
Marques Smalley <marquessmalley@gmail.com>
2025-08-05 09:18:26 -0400

    Added subsets for fonts

--- app/font.ts
+++ app/font.ts
@@ -1,10 +1,12 @@
 import { Bebas_Neue, Barlow_Condensed } from "next/font/google";
 
 export const bebasNeue = Bebas_Neue({
+  subsets: ["latin"],
   weight: "400",
 });
 
 export const barlowCondensed = Barlow_Condensed({
+  subsets: ["latin"],
   style: "normal",
   weight: "400",
 });
